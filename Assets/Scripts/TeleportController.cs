6872f96a53b502133c0041ff237f94572e810e0b
Matthew Agard <matthewa@inside.andrews.edu>
2017-10-03 16:36:33 -0400

    Initial commit

---
+++ Assets/Scripts/TeleportController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportController : MonoBehaviour {
+
+	public void SetGazedAt(bool gazedAt) {
+		// Highlight target when it's looked at
+		if (gazedAt == true)
+			GetComponent<Renderer> ().material.EnableKeyword ("_EMISSION");
+		else
+			GetComponent<Renderer> ().material.DisableKeyword ("_EMISSION");
+	}
+
+	// Use this for initialization
+	void Start () {
+		SetGazedAt(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
