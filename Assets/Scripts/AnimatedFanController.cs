292a24155eccc9de1100e71f0793a12d928e66a3
Matthew Agard <matthewa@inside.andrews.edu>
2017-11-08 14:46:15 -0500

    Begin Animated Fan

---
+++ Assets/Scripts/AnimatedFanController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatedFanController : MonoBehaviour {
+	private Animator anim;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator> ();
+	}
+
+	public void BladeRotation() {
+		anim.CrossFade ("BladeRotation", 1);
+	}
+
+	public void BladeHide() {
+		anim.CrossFade ("BladeHide", 1);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
