fdde220aecb5817a0873e345c95434b4db5e3d69
Matthew Agard <matthewa@inside.andrews.edu>
2017-11-08 21:04:24 -0500

    Fan Rotation and Hide

--- Assets/Scripts/AnimatedFanController.cs
+++ Assets/Scripts/AnimatedFanController.cs
@@ -4,18 +4,27 @@ using UnityEngine;
 
 public class AnimatedFanController : MonoBehaviour {
 	private Animator anim;
+	private Transform fanTransform = null;
+
+	public float fanHide = 0;
 
 	// Use this for initialization
 	void Start () {
 		anim = GetComponent<Animator> ();
+		fanTransform = transform.Find ("AnimatedFan");
 	}
 
 	public void BladeRotation() {
 		anim.CrossFade ("BladeRotation", 1);
 	}
 
-	public void BladeHide() {
-		anim.CrossFade ("BladeHide", 1);
+	public void FanHide() {
+		fanTransform.Translate (0, fanHide, 0);
+		StopCoroutine ("BladeRotation");
+	}
+
+	public void FanReappear() {
+		fanTransform.Translate (0, -fanHide, 0);
 	}
 
 	// Update is called once per frame
